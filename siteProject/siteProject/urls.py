d31f22b1ac66c579b7613bc629b1ae4a01508f78
SoULyDeV <josserand21@hotmail.com>
2023-05-30 19:55:31 +0400

    html.checkout

--- siteProject/siteProject/urls.py
+++ siteProject/siteProject/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views
 from django.urls import path
 
 #import views from our app 'core'
-from cart.views import add_to_cart, cart
+from cart.views import add_to_cart, cart, checkout
 from core.views import frontPage, shop, signup
 from product.views import product
 
@@ -18,6 +18,7 @@ urlpatterns = [
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
     path('cart/', cart, name='cart'),
+    path('cart/checkout', checkout, name='checkout'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),        
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
