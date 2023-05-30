b0cb88d8dc57ef2e84f022134d8c8cf3e9659751
SoULyDeV <josserand21@hotmail.com>
2023-05-30 18:43:07 +0400

    make the html for the cart

--- siteProject/siteProject/urls.py
+++ siteProject/siteProject/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views
 from django.urls import path
 
 #import views from our app 'core'
-from cart.views import add_to_cart
+from cart.views import add_to_cart, cart
 from core.views import frontPage, shop, signup
 from product.views import product
 
@@ -17,6 +17,7 @@ urlpatterns = [
     path('login', views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
+    path('cart/', cart, name='cart'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),        
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
